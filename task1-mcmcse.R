18adb2f0e02aa0c1213dabdd1a1f1322818fc8ee
theakhiljha <akhilkumarjha9@gmail.com>
2021-03-27 03:30:47 +0530

    added set.seed() function

--- task1-mcmcse.R
+++ task1-mcmcse.R
@@ -1,6 +1,9 @@
 #Easy
 #Task 1 - (i)
 
+# Adding set.seed() function in order to get same result while generating a sequence of random numbers.
+
+set.seed(100)
 library(mcmcse)
 foo <- rnorm(1e4)
 ess(foo)
@@ -11,3 +14,4 @@ rows = 10
 cols = 10
 B = matrix(rnorm(n=rows*cols), ncol=cols)
 eigen(B, only.values = TRUE)$values
+
